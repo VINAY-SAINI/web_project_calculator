36140996e716030a7d30b9e02eaf168a157f163a
VINAY <65601865+VINAY-SAINI@users.noreply.github.com>
2023-07-18 12:29:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let display = document.getElementById('screen');
+
+const wipe = () => {
+    display.value = '';
+}
+
+const show = (n) => { 
+    display.value += n;
+}
+const calc = () => {
+    display.value = eval(display.value);
+}
